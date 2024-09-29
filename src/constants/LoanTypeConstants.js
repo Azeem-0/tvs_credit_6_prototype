2fa08f625685e49297fb1d1b151d8216ab0c32af
SATHVIK SVS <svssathvik77@gmail.com>
2024-09-29 17:04:04 +0530

    added loan interest rates static decision table

--- src/constants/LoanTypeConstants.js
+++ src/constants/LoanTypeConstants.js
@@ -1,14 +1,13 @@
-export const loanTypes = [
-    "Choose Loan Type",
-    "Two Wheeler",
-    "Used Car",
-    "Consumer Durable",
-    "Mobile",
-    "Online Personal",
-    "Gold",
-    "Used Commercial Vehicle",
-    "Tractor",
-    "Property",
-    "Business",
-    "Three-Wheeler"
-];
+export const interestRateData = {
+    "Two Wheeler": [11, 12, 9], // Below 6 months, Below 1 year, More than 1 year
+    "Used Car": [13, 12, 9],
+    "Consumer Durable": [15, 13, 10],
+    "Mobile": [17, 12, 10],
+    "Online Personal": [15, 14, 9],
+    "Gold": [8, 7, 6],
+    "Used Commercial Vehicle": [13, 11, 8],
+    "Tractor": [10, 9, 6],
+    "Property": [8, 6, 4],
+    "Business": [14, 11, 8],
+    "Three-Wheeler": [12, 10, 8]
+};

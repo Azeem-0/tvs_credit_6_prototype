bb7e84069cb4f34d6f84a7e089a3ef49fa987b66
SATHVIK SVS <svssathvik77@gmail.com>
2024-09-29 16:47:33 +0530

    color consistency maintained

--- src/components/EmiInterface.jsx
+++ src/components/EmiInterface.jsx
@@ -25,15 +25,15 @@ const EmiInterface = () => {
 
     return (
         <div className='w-screen h-1/2 flex justify-center items-center'>
-            <div className=' w-2/3 h-full bg-[#8bd4a9] flex justify-between gap-2 rounded-2xl'>
-                <div className=' ml-6 w-1/4 h-full flex justify-center items-center bg-[#8bd4a9] rounded-tl-2xl rounded-bl-2xl'>
+            <div className=' w-2/3 h-full bg-[#EAF4F0] flex justify-between gap-2 rounded-2xl'>
+                <div className=' ml-6 w-1/4 h-full flex justify-center items-center rounded-tl-2xl rounded-bl-2xl'>
                     <form className='flex flex-col gap-12' action="" onSubmit={null}>
                         <input name='principal' className='custom-input p-2' type="number" onChange={changeInput} placeholder='Loan Amount' />
                         <input name='rate' className='custom-input p-2' type="number" onChange={changeInput} placeholder='Rate of Interest' />
                         <input name='tenure' className='custom-input p-2' type="number" onChange={changeInput} placeholder='Tenure (Months)' />
                     </form>
                 </div>
-                <div className='flex flex-col justify-center gap-4 w-3/4 p-6 h-full bg-[#8bd4a9] items-center rounded-tr-2xl rounded-br-2xl'>
+                <div className='flex flex-col justify-center gap-4 w-3/4 p-6 h-full items-center rounded-tr-2xl rounded-br-2xl'>
 
                     <select className='w-fit text-[0.785em] h-8 translate-x-165 self-end rounded-md bg-transparent outline-none' name="Loan Type" id="Loan Type">
                         {loanTypes.map((type, index) => (
@@ -43,27 +43,27 @@ const EmiInterface = () => {
                         ))}
                     </select>
 
-                    <div className='bg-[#108A43] w-full h-60 flex flex-wrap justify-around rounded-2xl'>
+                    <div className='bg-[#1BA024] w-full h-60 flex flex-wrap justify-center rounded-2xl p-6'>
                         <div className='w-1/2 flex justify-center items-center'>
-                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#0E7E3C] min-w-[12em] text-white flex-col'>
+                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#167E1B] min-w-[12em] text-white flex-col rounded-md'>
                                 <p>Monthly Emi</p>
                                 {emiInterface.monthly_emi && <p className=' font-bold'>₹ emiInterface.monthly_emi</p>}
                             </div>
                         </div>
                         <div className='w-1/2 flex justify-center items-center'>
-                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#0E7E3C] min-w-[12em] text-white flex-col'>
+                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#167E1B] min-w-[12em] text-white flex-col rounded-md'>
                                 <p>Prinicpal Amount</p>
                                 {emiInterface.principal && <p className=' font-bold'>₹ {emiInterface.principal}</p>}
                             </div>
                         </div>
                         <div className='w-1/2 flex justify-center items-center'>
-                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#0E7E3C] min-w-[12em] text-white flex-col'>
+                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#167E1B] min-w-[12em] text-white flex-col rounded-md'>
                                 <p>Total Intrest Payable</p>
                                 {emiInterface.total_interest && <p className=' font-bold'>₹ {emiInterface.total_interest}</p>}
                             </div>
                         </div>
                         <div className='w-1/2 flex justify-center items-center'>
-                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#0E7E3C] min-w-[12em] text-white flex-col'>
+                            <div className=' w-fit h-14 flex justify-center items-center px-4 bg-[#167E1B] min-w-[12em] text-white flex-col rounded-md'>
                                 <p>Total Amount Payable</p>
                                 {emiInterface.total_amount && <p className=' font-bold'>₹ {emiInterface.total_amount}</p>}
                             </div>

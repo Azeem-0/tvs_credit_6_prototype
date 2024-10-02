cacd2b6b6eb1aacc4d97c5b0ff8bdf57f1743963
Azeem Shaik <azeemshaik025@gmail.com>
2024-10-02 21:38:53 +0530

    update prediction utility

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import EmiInterface from "./components/EmiInterface.jsx";
 import UserFinanceContext from "./contexts/UserFinanceContext.jsx";
+import PredictionUtility from './utilities/PredictionUtility.jsx';
 // import UserFinanceContext from './components/UserFinanceContext';
 
 function App() {
   return (
     <div className='flex items-center justify-center w-screen h-screen'>
       <UserFinanceContext>
-        <EmiInterface />
+        {/* <EmiInterface /> */}
+        <PredictionUtility />
       </UserFinanceContext>
     </div>
   );
